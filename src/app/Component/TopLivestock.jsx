7ddc928319e255ecd3f856ad8f915cd0c2e3f2af
AhmadZaid20187 <ahmadzaid20187@gmail.com>
2026-05-02 16:56:22 +0600

    Top Livestock done

--- src/app/Component/TopLivestock.jsx
+++ src/app/Component/TopLivestock.jsx
@@ -1,23 +1,32 @@
 import React from 'react';
 import CardInfo from './CardInfo';
+import Link from 'next/link';
+import { Button } from '@heroui/react';
 
 const TopLivestock = async () => {
     const res = await fetch("http://localhost:3000/animal.json")
     const animal = await res.json()
-    const topAnimals = animal.slice(0, 6)
+    const topAnimals = animal.slice(0, 8)
     console.log(topAnimals)
 
 
     return (
-        <div className='container mx-auto'>
+        <div className='container mx-auto space-y-6 pb-6 '>
             <h2 className='text-6xl font-bold text-center'>Our Top Livestocks</h2>
 
-            <div className='grid grid-cols-1 min-[500px]:grid-cols-3 py-5 gap-5'>
+            <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 max-w-8xl mx-auto px-4'>
                 {topAnimals.map(animal => <CardInfo key={animal.id} animal={animal} />
 
                 )}
             </div>
 
+            <div className='flex justify-center pt-4'>
+                <Link href={"/all-animals"}>
+                    <Button className='bg-blue-500 hover:bg-blue-800 text-white px-8 py-3 rounded-full text-base font-semibold shadow-md hover:shadow-lg transition-all duration-300'>
+                        See All Animals →
+                    </Button>
+                </Link>
+            </div>
 
         </div>
     );
---
+++ src/app/all-animals/[id]/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AnimalDetails = () => {
+    return (
+        <div>
+            There will see animal details
+        </div>
+    );
+};
+
+export default AnimalDetails;
